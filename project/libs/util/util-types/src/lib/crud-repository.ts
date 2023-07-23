a742a9cea71b48719becd0234f40efcec0d75bc5
ruslanpro01 <ruslan91999@gmail.com>
2023-07-24 00:01:52 +0300

    feat: Добавит библиотеку с общими типами и создась интерфейс CRUD репоизитория

---
+++ project/libs/util/util-types/src/lib/crud-repository.ts
@@ -0,0 +1,6 @@
+export interface CRUDRepository<Entity, Id, Return> {
+  create(entity: Entity): Promise<Return>;
+  remove(id: Id): Promise<void>;
+  update(id: Id, entity: Entity): Promise<Return>;
+  findById(id: Id): Promise<Return>;
+}
