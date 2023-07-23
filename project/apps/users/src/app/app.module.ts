655c6626fc82145127fe787b810a4104e41bf7f0
ruslanpro01 <ruslan91999@gmail.com>
2023-07-23 22:52:15 +0300

    chore: Добавит заготовки для контроллера и сервиса для модуля аутентификации

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TasksUserModule } from './tasks-user/tasks-user.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [TasksUserModule, AuthenticationModule],
   controllers: [],
   providers: [],
 })
