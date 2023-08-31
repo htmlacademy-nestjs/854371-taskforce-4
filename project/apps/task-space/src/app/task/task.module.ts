fc7adbfed7e5941b7bef37e193f2895c821856db
ruslanpro01 <ruslan91999@gmail.com>
2023-08-31 15:24:26 +0300

    fix: Исправит ошибку запуску task-space

--- project/apps/task-space/src/app/task/task.module.ts
+++ project/apps/task-space/src/app/task/task.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 import { TaskRepository } from './task.repository';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
-  imports: [],
+  imports: [CategoryModule],
   controllers: [TaskController],
   providers: [TaskService, TaskRepository],
   exports: [TaskRepository]
