a2603c71507844ff0712837ce0753b63474a51c5
Ruslan Shevchenko <ruslan91999@gmail.com>
2023-09-01 16:45:34 +0300

    feat: Добавит сохранение информации о загруженных файлах в базу данных

--- project/apps/uploader/src/app/file/file.controller.ts
+++ project/apps/uploader/src/app/file/file.controller.ts
@@ -1,10 +1,12 @@
 import { FileService } from './file.service';
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import 'multer';
+import { fillObject } from '@project/util/util-core';
+import { UploadedFileRdo } from './rdo/uploaded-file.rdo';
 
-@Controller()
+@Controller('files')
 export class FileController {
   constructor(
     private readonly fileService: FileService
@@ -15,5 +17,12 @@ export class FileController {
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     const newFile = await this.fileService.save(file);
+    return fillObject(UploadedFileRdo, newFile);
+  }
+
+  @Get(':fileId')
+  async getFile(@Param('fileId') fileId: string) {
+    const existFile = await this.fileService.getFile(fileId)
+    return fillObject(UploadedFileRdo, existFile);
   }
 }
