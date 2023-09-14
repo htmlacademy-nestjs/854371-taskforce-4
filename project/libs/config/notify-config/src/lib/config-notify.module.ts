a1b8eb5dffd3267757210d2f7cc245044b9e85d4
Ruslan Shevchenko <ruslan91999@gmail.com>
2023-09-14 21:35:25 +0300

    Оптимизирует импорты

--- project/libs/config/notify-config/src/lib/config-notify.module.ts
+++ project/libs/config/notify-config/src/lib/config-notify.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import notifyConfig from './config/notify.config';
 
-const envFilePath = 'apps/notify/.env';
-
 @Module({
   imports: [ ConfigModule.forRoot({
     isGlobal: true,
